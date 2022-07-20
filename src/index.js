d2df64d01d0c9df5e042cceebaa0487d020f6561
Michelle Caraballo <98852513+caraballomp@users.noreply.github.com>
2022-07-19 18:11:47 -0700

    star update section

--- src/index.js
+++ src/index.js
@@ -8,15 +8,22 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h2>Star Components:</h2>
       <Star color="pink" isFilled />
-      <Star color="magenta" />
-      <Star color="indigo" isFilled />
-      <h2>Rating Component (uses Stars):</h2>
-      <Rating stars={4} />
+      <Star color="blue" />
+      <Star color="yellow" isFilled />
+      <h3>Rating component (uses Stars):</h3>
+      <Rating stars={0} />
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+// <div className="App">
+//   <h2>Star Components:</h2>
+//   <Star color="pink" isFilled />
+//   <Star color="magenta" />
+//   <Star color="indigo" isFilled />
+//   <h2>Rating Component (uses Stars):</h2>
+//   <Rating stars={4} />
